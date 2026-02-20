23fe368eec089b97e4016591ec18d7f8bc7124b1
Puxadasvip <138346008+Puxadasvip@users.noreply.github.com>
2026-02-20 01:29:26 -0300

    bot.php

--- bot.php
+++ bot.php
@@ -8,10 +8,10 @@ const LOG_FILE      = __DIR__ . '/bot.log';
 const CONSULTAS_DIR = __DIR__ . '/consultas';
 
 // @ do bot (sem @ aqui)
-const BOT_USERNAME  = 'EmonNullbot';
+const BOT_USERNAME  = 'Puxadasvip_bot';
 
 // 👑 ID do dono/admin do bot (sem restrições de antiflood)
-const ADMIN_ID = 7505318236;
+const ADMIN_ID = 1735827798;
 
 // Auto-delete em grupos (segundos)
 if (!defined('AUTO_DELETE_SECONDS')) define('AUTO_DELETE_SECONDS', 60);
@@ -777,7 +777,7 @@ function keyboard_main_private(int $ownerId): array {
       ],
       // ✅ SUPORTE
       [
-        ['text'=>'🆘 Suporte','url'=>'https://t.me/GonzalesDev']
+        ['text'=>'🆘 Suporte','url'=>'https://t.me/LeoIptv1']
       ]
     ]
   ];
@@ -791,7 +791,7 @@ function keyboard_main_group(int $ownerId): array {
       ],
       [
         ['text'=>'🪪 Consultas',"callback_data"=>"MENU_CONSULTAS|$ownerId"],
-        ['text'=>'📢 Canal','url'=>'https://t.me/GonzalesCanal']
+        ['text'=>'📢 Canal','url'=>'https://t.me/puxadasgratis21']
       ]
     ]
   ];
@@ -3282,5 +3282,6 @@ Realize consultas completas com rapidez e total segurança.
 } catch (Throwable $e) {
   logx('Exception: '.$e->getMessage());
 }
+
 
 echo 'ok';
